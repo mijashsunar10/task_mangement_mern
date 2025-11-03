88a48ebd5da73c033d1376686baebabea89a38e7
mijashsunar10 <mijashsunar.xdezo@gmail.com>
2025-11-03 11:18:17 +0545

    Here now thebackend is configured and evrything is set up

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Backend
+# npm init -y 
+# npm i express dotenv body-parser cors mongoose
+express = main framework for buliding webserver and APIS in node.js
+
+dotenv = loads environment vaibable form a .env file into process.env
+
+body-parser = parses the incoming request body
+Express doesn’t handle request bodies by default, so body-parser helps convert raw request data into a readable JavaScript object.
+
+cors = allows frontend and backend to make API requests
+
+moongoose = mongodb object modeling tool for Node.js
+
---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express  = require('express'); // Importing express module
+const app = express(); //Express lai initialize garera app variable ma rakhne
+const dotenv = require('dotenv')
+dotenv.config(); //jati ni key value pair hunxa .env file ma yesko help bata memory ma load hunxa
+
+const PORT = process.env.PORT || 3000; //process.env..... yo bata chai j ni key xa teslai access garna sakinxa
+
+app.get('/', (req, res)=>{
+    res.send('Hello World from Express');
+});
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
