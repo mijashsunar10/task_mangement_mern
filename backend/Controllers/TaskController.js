65df7c4f25797b258bb17baaafa0deb31e99e67c
mijashsunar10 <mijashsunar.xdezo@gmail.com>
2025-11-03 13:24:09 +0545

    Update is completed

--- backend/Controllers/TaskController.js
+++ backend/Controllers/TaskController.js
@@ -30,11 +30,47 @@ const fetchAllTask = async(req,res)=>
     }
     catch(err)
     {
-        res.status(500).json({message: "Server Error", error: err.message});
+        res.status(500).json({message: "failed to update Task ", error: err.message});
     }
 }
 
 
-    module.exports = {createTask, fetchAllTask
+const updateTask = async(req,res)=>
+{
+    // const data = req.body;
+
+    try{
+
+        const id = req.params.id; //id nikalni
+        const body = req.body; //body ko extract garni
+        const obj = {$set:{...body}}; //update garni object chainxa mongodb mA so object lai convert gardai xam and extract body
+        await TaskModel.findByIdAndUpdate(id, obj); //id ra update garni object pass garni
+        res.status(200).json({message: "Task Updated", success:true});
+
+    }
+    catch(err)
+    {
+        res.status(500).json({message: "Server Error", error: err.message});
+    }
+}
+
+const deleteTask = async(req,res)=>
+{
+    const data = req.body;
+
+    try{
+
+        const model = new TaskModel(data);
+        await model.save();
+        res.status(201).json({message: "Task created successfully", success:true});
+
+    }
+    catch(err)
+    {
+        res.status(500).json({message: "Server Error", error: err.message});
+    }
+}
+
+    module.exports = {createTask, fetchAllTask, updateTask
 
 }
--- backend/Routes/TaskRouter.js
+++ backend/Routes/TaskRouter.js
@@ -3,15 +3,23 @@ const router = express.Router();
 const TaskModel = require('../Models/TaskModel');
 
 
-const {createTask, fetchAllTask} = require('../Controllers/TaskController');
+const {createTask, fetchAllTask, updateTask} = require('../Controllers/TaskController');
 
+
+//to fetch all tasks
 router.get('/',  fetchAllTask
 )
 
-//to create a task
 
+//to create a task
 router.post('/', createTask);
 
+//to update a task
+
+router.put('/:id',updateTask)
+
+
+//to delete  task
 
 
 
